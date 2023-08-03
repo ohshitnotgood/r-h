6219ea35c5eca1e904937001b836513f8d242fed
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2023-08-03 12:13:57 +0200

    Autowidth change now works for the columns

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,43 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
-    const [d1Class, setD1Class] = createSignal("col w-full")
-    const [d2Class, setD2Class] = createSignal("col w-[100px]")
-    const [d3Class, setD3Class] = createSignal("col w-[100px]")
-    const [d4Class, setD4Class] = createSignal("col w-[100px]")
-    const [d5Class, setD5Class] = createSignal("col w-[100px]")
+    const [d1Class, setD1Class] = createSignal("col col-full")
+    const [d2Class, setD2Class] = createSignal("col col-fit")
+    const [d3Class, setD3Class] = createSignal("col col-fit")
+    const [d4Class, setD4Class] = createSignal("col col-fit")
+    const [d5Class, setD5Class] = createSignal("col col-fit")
 
     function resetAllWidthToDefault() {
-        setD1Class("col w-[100px]")
-        setD2Class("col w-[100px]")
-        setD3Class("col w-[100px]")
-        setD4Class("col w-[100px]")
-        setD5Class("col w-[100px]")
+        setD1Class("col col-fit")
+        setD2Class("col col-fit")
+        setD3Class("col col-fit")
+        setD4Class("col col-fit")
+        setD5Class("col col-fit")
     }
 
     function onClickD1() {
         resetAllWidthToDefault()
-        setD1Class("col w-full")
+        setD1Class("col col-full")
     }
 
     function onClickD2() {
         resetAllWidthToDefault()
-        setD2Class("col w-full")
+        setD2Class("col col-full")
     }
 
     function onClickD3() {
         resetAllWidthToDefault()
-        setD3Class("col w-full")
+        setD3Class("col col-full")
     }
 
     function onClickD4() {
         resetAllWidthToDefault()
-        setD4Class("col w-full")
+        setD4Class("col col-full")
     }
 
     function onClickD5() {
         resetAllWidthToDefault()
-        setD5Class("col w-full")
+        setD5Class("col col-full")
     }
 
 
