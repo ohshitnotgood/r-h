629d52f5c416c28a6b3138c5aa0b279fe02a1d7a
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2023-08-11 16:59:19 +0200

    momentum scroll works at full width

--- src/components/reusable/MScroll.tsx
+++ src/components/reusable/MScroll.tsx
@@ -1,64 +1,83 @@
-import {onMount} from "solid-js";
+import {createSignal, onMount} from "solid-js";
+import {style} from "solid-js/web";
 
 export function MScroll(props: { class?: string, style?: string }) {
+    let root: HTMLDivElement
     let main: HTMLDivElement
+
     let oldScroll: number = 0
 
-    // onMount (() => {
-    //     main.style.position = 'fixed'
-    //     main.style.top = 0
-    //     main.style.left = 0
-    // })
+    const [mainStyle, setMainStyle] = createSignal(`position: fixed; top: 0; left: 0;`)
 
 
     let dy = 0, dx = 0
+    let sx = 0, sy = 0
 
     function render() {
-        // We calculate our container position by using our Linear Interpolation method.
-        dx = lerp(dx, sx, 0.05);
-        dy = lerp(dy, sy, 0.05);
+        // console.log("render")
+        dx = linearInterpolate(dx, sx, 0.05);
+        dy = linearInterpolate(dy, sy, 0.05);
 
         dx = Math.floor(dx * 100) / 100;
         dy = Math.floor(dy * 100) / 100;
 
-        console.log(dy);
+        setMainStyle(`position: fixed; top: 0; left: 0; transform: translate(0, -${dy}px)`)
 
-        // Finally we translate our container to its new positions.
-        // Don't forget to add a minus sign because the container needs to move in
-        // the opposite direction of the window scroll.
-        main.style.transform = `translate(-${dx}px, -${dy}px)`;
+        window.requestAnimationFrame(render)
     }
 
-// This is our Linear Interpolation method.
+
+    window.requestAnimationFrame(render)
+
     function linearInterpolate(a, b, n) {
         return (1 - n) * a + n * b;
     }
 
 
     function onScroll(e: Event) {
-        console.log("scrolling")
-        if (main.scrollTop < oldScroll) console.log("scrolling up")
-        if (main.scrollTop > oldScroll) console.log("scrolling down")
-        oldScroll = main.scrollTop
+        console.log(root.scrollTop)
+        if (root.scrollTop < oldScroll) console.log("scrolling up")
+        if (root.scrollTop > oldScroll) console.log("scrolling down")
+        oldScroll = root.scrollTop
 
-        // dx = linearInterpolate(dx, sx, 0.05);
-        // dy = linearInterpolate(dy, sy, 0.05);
-        //
-        // dx = Math.floor(dx * 100) / 100;
-        // dy = Math.floor(dy * 100) / 100;
-        //
-        // main.style.transform = `translate(-${dx}px, -${dy}px)`;
+        sx = root.scrollLeft
+        sy = root.scrollTop
     }
 
     return (
-        <div class={`w-screen h-screen overflow-scroll relative`} onscroll={onScroll} >
-            <div class={`fixed top-0 left-0`} ref={main}>
-                {props.children}
+        <>
+            <style>{`
+                .adam {
+                    z: 20;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    opacity: 0;    
+                }
+                
+                .adam::-webkit-scrollbar {
+                    display: none;
+                }
+                
+                .hide-scrollbar::-webkit-scrollbar {
+                    display: none;
+                }
+            `}</style>
+
+
+            <div class={`w-[50vw] h-screen`}>
+                <div class={`w-full h-full overflow-y-scroll relative hide-scrollbar`}  ref={root} onscroll={onScroll} >
+                    <div style={mainStyle()} >
+                        <div ref={main}>
+                            {props.children}
+                        </div>
+                    </div>
+                    <div class="adam">
+                        {props.children}
+                    </div>
+                </div>
             </div>
-            <div class={`z-20 absolute top-0 left-0 opacity-0`}>
-                f
-                {props.children}
-            </div>
-        </div>
+
+        </>
     )
 }
