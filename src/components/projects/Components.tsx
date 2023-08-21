da8396ac7e39430173a4596ba3b58dc9bc404b1d
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2023-08-21 12:45:19 +0200

    changed all cards in project to PrimaryText

--- src/components/projects/Components.tsx
+++ src/components/projects/Components.tsx
@@ -8,24 +8,19 @@ export function TennisForTwoCard( props: {xSetter: any, ySetter: any, setTextOnH
     return (
         <Card>
             <h1>
-                {/*<button class={`flex hover:underline items-center flex-row pt-0 decoration-[color:var(--primary-color-post)]`} onclick={() => window.location.href="https://github.com/ohshitnotgood/ois-hw-tracker"}>*/}
-                {/*    <PrimaryText>Tennis For Two</PrimaryText>*/}
-                {/*    <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-                {/*</button>*/}
-
                 <A2 href={`https://github.com/ohshitnotgood/ois-hw-tracker`} hoverTextSetter={props.setTextOnHover} xSetter={props.xSetter} ySetter={props.ySetter}>
                     Tennis for Two
                 </A2>
 
             </h1>
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     Tennis-for-Two, an ungraded college project with my colleague Alin-Cristian
                     Serban at KTH, revives the 1958 game on an Arduino microcontroller.
                     Players control in-game rackets via a phone app, tracking gyroscope data
                     sent to an ESP32 and then transmitted to the MIPS-microprocessor. SPI
                     development paused due to hardware constraints; set to resume in November.
-                </SecondaryText>
+                </PrimaryText>
             </p>
 
             <PrimaryText>
@@ -69,22 +64,17 @@ export function SwiftEssentialsCard( props: {xSetter: any, ySetter: any, setText
     return(
         <Card>
             <h1 class={`hover:underline`}>
-                {/*<button class={`flex items-center flex-row pt-0 decoration-[color:var(--primary-color-post)]`} onclick={() => window.location.href="https://github.com/ohshitnotgood/essentials"}>*/}
-                {/*    <PrimaryText>Swift Essentials</PrimaryText>*/}
-                {/*    <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-                {/*</button>*/}
-
                 <A2 href={`https://github.com/ohshitnotgood/essentials`} hoverTextSetter={props.setTextOnHover} xSetter={props.xSetter} ySetter={props.ySetter}>
                     Swift Essentials
                 </A2>
             </h1>
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     A Python script uses the command line to convert binary inputs into
                     their hexadecimal equivalents swiftly and efficiently.
                     While studying in a course involving processor architecture, this Python script
                     was conceived to verify on-paper conversions between hexadecimal and binary codes.
-                </SecondaryText>
+                </PrimaryText>
             </p>
         </Card>
     )
@@ -94,31 +84,24 @@ export function EyeTrackerCard( props: {xSetter: any, ySetter: any, setTextOnHov
     return (
         <Card>
             <h1 class={`hover:underline`}>
-                {/*<button class={`flex hover:underline items-center flex-row pt-0 decoration-[color:var(--primary-color-post)]`}*/}
-                {/*        onclick={() => window.location.href="https://github.com/ohshitnotgood/eye-allergy.git"}>*/}
-                {/*    <PrimaryText>Allergy Tracker</PrimaryText>*/}
-                {/*    <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-                {/*</button>*/}
-
-
                 <A2 href={`https://github.com/ohshitnotgood/eye-allergy.git`} hoverTextSetter={props.setTextOnHover} xSetter={props.xSetter} ySetter={props.ySetter}>
                     Tennis for Two
                 </A2>
             </h1>
 
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     A simple macOS app, built using SwiftUI, Swift Graphs and FileManager to track events of itches during a period I contracted eye allergies.
                     This app would log data of eye itches over a period of time to aid in recording the effectiveness of the medicines received from the doctor.
                     The graph displayed was a simple SwiftUI graph
-                </SecondaryText>
+                </PrimaryText>
             </p>
 
             <PrimaryText>
                 <p>
                     <em>Frameworks used</em>
-                    <a>SwiftUI</a>
-                    <a>Swift Graphs</a>
+                    <A href={`https://developer.apple.com/SwiftUI`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.setTextOnHover}>SwiftUI</A>
+                    <A href={`https://developer.apple.com/documentation/charts`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.setTextOnHover}>Swift Graphs</A>
                 </p>
             </PrimaryText>
         </Card>
@@ -129,11 +112,6 @@ export function MomentumScrollCard( props: {xSetter: any, ySetter: any, setTextO
     return (
         <Card>
             <h1>
-                {/*<button class={`flex hover:underline items-center flex-row decoration-[color:var(--primary-color-post)]`}>*/}
-                {/*    <PrimaryText>solid-momentum-scroll</PrimaryText>*/}
-                {/*    <img src={disableLink} alt={"Unavailable link icon"} class={`h-5 pl-2`}/>*/}
-                {/*</button>*/}
-
                 <A2 href={`no project link available yet`} hoverTextSetter={props.setTextOnHover} xSetter={props.xSetter} ySetter={props.ySetter}>
                     solid-momentum-scroll
                 </A2>
@@ -141,11 +119,11 @@ export function MomentumScrollCard( props: {xSetter: any, ySetter: any, setTextO
             </h1>
 
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     A SolidJS plugin that integrates momentum scrolling and parallax effects into webpage
                     scrolling behavior. During website development, innovative approaches were employed
                     for momentum scrolling, with ongoing testing prior to its SolidJS Ecosystem release.
-                </SecondaryText>
+                </PrimaryText>
             </p>
         </Card>
     )
@@ -154,14 +132,11 @@ export function MomentumScrollCard( props: {xSetter: any, ySetter: any, setTextO
 export function SchedulerCard( props: { xSetter: any, ySetter: any, textSetter: any}) {
     return (
         <Card>
-            {/*<h1>*/}
-            {/*    <PrimaryText>Scheduler</PrimaryText>*/}
-            {/*</h1>*/}
             <A2 href={`no project link available yet`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>
                 Scheduler
             </A2>
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     An exceptionally adaptable and remarkably swift online questionnaire
                     platform for KTH's utilization. The platform will introduce
                     "Routes" that will change subsequent queries based on earlier responses.
@@ -170,7 +145,7 @@ export function SchedulerCard( props: { xSetter: any, ySetter: any, textSetter: 
                     into multiple-choice selections. The application is set to feature various
                     themes, with plans to incorporate a SQL-style query language and
                     graphs for enhanced control over data visualization.
-                </SecondaryText>
+                </PrimaryText>
             </p>
 
             <PrimaryText>
@@ -178,12 +153,12 @@ export function SchedulerCard( props: { xSetter: any, ySetter: any, textSetter: 
                     <em>
                         Frameworks used:
                     </em>
-                    <a href={`https://www.solidjs.com/`}>SolidJS</a>
-                    <a href={`https://start.solidjs.com/getting-started/what-is-solidstart`}>SolidStart</a>
-                    <a href={`https://www.typescriptlang.org/`}>TypeScript</a>
-                    <a href={`https://tailwindcss.com/docs/installation`}>TailwindCSS</a>
-                    <a href={`https://www.prisma.io/docs`}>Prisma</a>
-                    <a href={`https://www.postgresql.org/`}>Postgresql</a>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://www.solidjs.com/`}>SolidJS</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://start.solidjs.com/getting-started/what-is-solidstart`}>SolidStart</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://www.typescriptlang.org/`}>TypeScript</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://tailwindcss.com/docs/installation`}>TailwindCSS</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://www.prisma.io/docs`}>Prisma</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href={`https://www.postgresql.org/`}>Postgresql</A>
                 </p>
             </PrimaryText>
         </Card>
@@ -193,36 +168,30 @@ export function SchedulerCard( props: { xSetter: any, ySetter: any, textSetter: 
 export function HomeworkCard(props: {xSetter: any, ySetter: any, textSetter: any}) {
     return (
         <Card>
-            {/*<h1>*/}
-            {/*    <button class={`flex hover:underline items-center flex-row`} onclick={() => window.location.href="https://github.com/ohshitnotgood/ois-hw-tracker"}>*/}
-            {/*        <PrimaryText>Homework Tracker</PrimaryText>*/}
-            {/*        <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-            {/*    </button>*/}
-            {/*</h1>*/}
             <A2 href={`https://github.com/ohshitnotgood/ois-hw-tracker`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>
                 Homework Tracker
             </A2>
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     During my time in high school, I developed an Android application aimed at
                     tracking homework tasks. As its name implies, the app's purpose was to
                     maintain a log of assignment deadlines. Additionally, the app would generate
                     personalized automatic alerts for users. What set this application apart from a
                     basic calendar reminder was its incorporation of distinct fields, such as
                     textbook page numbers and dates for referencing past papers.
-                </SecondaryText>
+                </PrimaryText>
             </p>
 
             <PrimaryText>
                 <p>
                     <em>Frameworks used</em>
-                    <a>Android Studio</a>
-                    <a href={'https://developer.android.com/docs'}>
+                    <A href={`https://developer.android.com/studio`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>Android Studio</A>
+                    <A href={'https://developer.android.com/docs'} ySetter={props.ySetter} xSetter={props.xSetter} hoverTextSetter={props.textSetter}>
                         AndroidSDK
-                    </a>
-                    <a>Kotlin</a>
-                    <a>LiveData</a>
-                    <a>MotionLayout</a>
+                    </A>
+                    <A href={`https://kotlinlang.org`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>Kotlin</A>
+                    <A href={`https://developer.android.com/topic/libraries/architecture/livedata`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>LiveData</A>
+                    <A href={`https://developer.android.com/develop/ui/views/animations/motionlayout`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>MotionLayout</A>
                 </p>
             </PrimaryText>
         </Card>
@@ -232,31 +201,25 @@ export function HomeworkCard(props: {xSetter: any, ySetter: any, textSetter: any
 export function HexcomCard(props: {xSetter: any, ySetter: any, textSetter: any}) {
     return (
         <Card>
-            {/*<h1 class={`hover:underline`}>*/}
-            {/*    <button class={`flex items-center flex-row`} onclick={() => window.location.href="https://github.com/ohshitnotgood/essentials"}>*/}
-            {/*        <PrimaryText>hexcom</PrimaryText>*/}
-            {/*        <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-            {/*    </button>*/}
-            {/*</h1>*/}
             <A2 href={`https://github.com/ohshitnotgood/hexcom`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>
                 hexcom
             </A2>
             <p>
-                <SecondaryText>
+                <PrimaryText>
                     A Python script uses the command line to convert binary inputs into
                     their hexadecimal equivalents swiftly and efficiently.
                     While studying in a course involving processor architecture, this Python script
                     was conceived to verify on-paper conversions between hexadecimal and binary codes.
-                </SecondaryText>
+                </PrimaryText>
             </p>
 
             <p>
                 <PrimaryText>
                     <em>Frameworks used</em>
-                    <a>Python3</a>
-                    <a>rich</a>
-                    <a>Python ArgParser</a>
-                    <a>Bash Script</a>
+                    <A href={`https://docs.python.org/3/`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>Python3</A>
+                    <A href={`https://github.com/Textualize/rich`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>rich</A>
+                    <A href={`https://docs.python.org/3/library/argparse.html`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>Python ArgParser</A>
+                    <A href={`https://en.wikipedia.org/wiki/Shell_script`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>Bash Script</A>
                 </PrimaryText>
             </p>
         </Card>
@@ -267,15 +230,6 @@ export function HexcomCard(props: {xSetter: any, ySetter: any, textSetter: any})
 export function ThisWCard(props: {xSetter: any, ySetter: any, textSetter: any}) {
     return (
         <Card>
-            {/*<h1>*/}
-            {/*    <button class={`flex hover:underline items-center flex-row`} onclick={() => window.location.href="https://github.com/ohshitnotgood/r-h"}>*/}
-            {/*        <code>*/}
-            {/*            <PrimaryText>this.website</PrimaryText>*/}
-            {/*        </code>*/}
-            {/*        <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>*/}
-            {/*    </button>*/}
-            {/*</h1>*/}
-
             <A2 href={`https://github.com/ohshitnotgood/r-h`} xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter}>
                 <code>this.website</code>
             </A2>
@@ -288,10 +242,12 @@ export function ThisWCard(props: {xSetter: any, ySetter: any, textSetter: any}) 
                 </PrimaryText>
             </p>
             <p>
-                <em>Frameworks used:</em>
-                <a href="https://solidjs.com">SolidJS</a>
-                <a href="https://tailwindcss.com">TailwindCSS</a>
-                <a href="https://vercel.com">Vercel</a>
+                <PrimaryText>
+                    <em>Frameworks used:</em>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href="https://solidjs.com">SolidJS</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href="https://tailwindcss.com">TailwindCSS</A>
+                    <A xSetter={props.xSetter} ySetter={props.ySetter} hoverTextSetter={props.textSetter} href="https://vercel.com">Vercel</A>
+                </PrimaryText>
             </p>
 
         </Card>
@@ -309,7 +265,7 @@ function A(props: {href: string, children: any, xSetter: any, ySetter: any, hove
     }
 
     return (
-        <a onmousemove={(e) => mouseMoveAction(props.xSetter, props.ySetter, e)} onmouseleave={() => props.hoverTextSetter('null')} class={`inline-block cursor-pointer rounded-3xl border border-[color:var(--border-color)] px-2`} href={props.href}>
+        <a onmousemove={(e) => mouseMoveAction(props.xSetter, props.ySetter, e)} onmouseleave={() => props.hoverTextSetter('null')} class={`inline-block primary-text cursor-pointer rounded-3xl border border-[color:var(--border-color)] px-2`} href={props.href}>
             <SecondaryText>{props.children}</SecondaryText>
         </a>
     )
@@ -327,7 +283,7 @@ function A2( props: {href: string, children: any, xSetter: any, ySetter: any, ho
     }
 
     return (
-        <a onmousemove={(e) => mouseMoveAction(props.xSetter, props.ySetter, e)} onmouseleave={() => props.hoverTextSetter('null')} class={`rounded-none border-none font-medium text-lg p-0 m-0 flex hover:underline items-center flex-row pt-0 decoration-[color:var(--primary-color-post)]`} href={props.href}>
+        <a onmousemove={(e) => mouseMoveAction(props.xSetter, props.ySetter, e)} onmouseleave={() => props.hoverTextSetter('null')} class={`rounded-none primary-text border-none font-medium text-lg p-0 m-0 flex hover:underline items-center flex-row pt-0 decoration-[color:var(--primary-color-post)]`} href={props.href}>
             <SecondaryText>{props.children}</SecondaryText>
             <img src={linkIcon} alt={"Link icon"} class={`h-5 pl-2`}/>
         </a>
